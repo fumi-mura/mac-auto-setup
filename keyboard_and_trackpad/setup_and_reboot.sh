74465446824661b89defa06b25906dca7171ee85
FumiakiMuramatsu <fumiakimuramatsu@FumiakiMuramatsunonotobukkukonpyuta.local>
2024-06-30 17:42:29 +0900

    add appearance

--- keyboard_and_trackpad/setup_and_reboot.sh
+++ keyboard_and_trackpad/setup_and_reboot.sh
@@ -1,20 +1,28 @@
 #!/bin/sh
 
+# Appearance
+
+## Set to dark mode
+defaults write NSGlobalDomain AppleInterfaceStyle -string "Dark"
+
+## Set sidebar icon size to small
+defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 1
+
 # keyboard
 
-# Repeat speed.
+## Repeat speed.
 defaults write -g KeyRepeat -int 2
 
-# Time to repeat input recognition.
+## Time to repeat input recognition.
 defaults write -g InitialKeyRepeat -int 15
 
 # trackpad
 
-# Tap to allow clicks.
+## Tap to allow clicks.
 defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -int 1
 
-# Change cursor movement speed.
+## Change cursor movement speed.
 defaults write -g com.apple.trackpad.scaling -float 3
 
 # Reboot is required for this settings to reflect.
