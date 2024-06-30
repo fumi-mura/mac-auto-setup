9854305364debd361703cb6c4a57a6d0a0a48811
FumiakiMuramatsu <fumiakimuramatsu@FumiakiMuramatsunonotobukkukonpyuta.local>
2024-06-30 16:36:25 +0900

    control rock screen time

---
+++ rock_screen/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Set display sleep time on battery use
+sudo pmset -b displaysleep 10
+
+# GUI does not change, but it is reflected, and can be checked with the following command
+# pmset -g
