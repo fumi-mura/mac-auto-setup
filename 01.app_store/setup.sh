d577ec883621d79473cad0b8550ee05d7925a899
Fumi-Mura <381704fumi@gmail.com>
2025-05-31 16:15:44 +0900

    Add app store applications

---
+++ 01.app_store/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# For can't install apps from App Store.
+brew install mas
+
+# App Store applications.
+mas install 1429033973 # RunCat
+
+mas install 441258766 # Magnet
+
+mas install 6447125648 # Gestimer 2
